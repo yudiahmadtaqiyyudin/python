aaaeb4002cc36b6e83d24cc44671a0467b64cb00
yudiahmadtaqiyyudin <yudiahmadtaqiyyudin@gmail.com>
2025-02-21 22:11:37 +0700

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,97 @@
+x = 5
+print(x)
+
+x = "Sally"
+print(x)
+
+a = int(3.5)
+print(a)
+
+b = float(5)
+print(b)
+
+c = int("10")
+print(c)
+
+d = bool(0)
+print(d)
+
+e = 3
+print(type(e))
+
+f = "Mobil"
+print(type(f))
+
+g = [1, 2, 3, 4, 5]
+print(type(g))
+
+h = (1, 2, 3)
+print(type(h))
+
+i = {1, 2, 3}
+print(type(i))
+
+j = {"nama" : "ali"}
+print(type(j))
+
+k = "it's a beautifull day"
+print(k)
+
+l = 'it\'s beautifull day'
+print(l)
+
+p = 5
+P = "sally"
+print(p)
+print(P)
+
+_myNumber1 = 5
+print(_myNumber1)
+
+aku, kamu, dia = 1, 2, 3
+print(aku, kamu, dia)
+
+me = you = him = 100
+print(me, you, him)
+
+ss = ["apple", "banana", "cherry"]
+aa, bb, cc = ss
+
+print(aa)
+print(bb)
+print(cc)
+
+sss = {"name" : "lala", "umur" : "12", "alamat" : "kp.ubur ubur"}
+dd, ff, gg = sss.values()
+print(dd)
+print(ff)
+print(gg)
+
+print('hello', 'world')
+
+ww = ' awesome'
+def myFunction():
+    ww = ' fantastic'
+    print('Python is' + ww)
+
+myFunction()
+print('Python is' + ww)
+
+def myFunc():
+    global qq 
+    qq = 'saya sedang belajar python'
+
+myFunc()
+print(qq)
+
+str = 'hello world'
+str2 = "hello world"
+str3 = '''hello
+        world'''
+str4 = """hello
+        world"""
+
+print(str)
+print(str2)
+print(str3)
+print(str4)
