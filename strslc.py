c3b0962ec17bdb748e716120271ef3b8792b27d5
yudiahmadtaqiyyudin <yudiahmadtaqiyyudin@gmail.com>
2025-02-22 20:51:45 +0700

    Create strslc.py

---
+++ strslc.py
@@ -0,0 +1,14 @@
+slc = '1234-5678-9012-3456'
+
+# notasi > slc[start:end]
+print(slc[0])
+print(slc[5:])
+print(slc[:9])
+print(slc[5:14])
+
+print(slc[-4:-1])
+print(slc[:-5])
+print(slc[-4:])
+
+# pahami terutama dibagian negative indexing
+# pahami di bagian notasi end, untuk nilai index end tidak diambil, hanya sampai sebelum nilai index end, berlaku juga untuk negative indexing (untuk lebih jelas bisa dilihat di myDocs org.pad)
