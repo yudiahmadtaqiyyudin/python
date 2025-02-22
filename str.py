d7f0158d91bbb2cef7b76daebdd0f23ca9c10698
yudiahmadtaqiyyudin <yudiahmadtaqiyyudin@gmail.com>
2025-02-22 20:12:04 +0700

    Create str.py

---
+++ str.py
@@ -0,0 +1,42 @@
+x = """
+    hello
+            disini saya 
+                         sedang belajar python string 
+                         
+                         variable"""
+
+print(x)
+
+str = 'hello'
+
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+
+xx = "banana"
+for x in xx:
+    print(x)
+
+a = 'hello world'
+print(len(a))
+
+text = "i am learning python"
+print("python" in text)
+print("java" in text)
+print("a" in text)
+print("z" in text)
+
+text2 = 'saya sedang belajar java'
+print('python' not in text2)
+print('java' not in text2)
+
+text3 = 'saya sedang belajar html'
+if 'html' in text3:
+    print('saya sedang belajar')
+
+if 'html' not in text3:
+    print('hello')
+else:
+    print('heyy')
